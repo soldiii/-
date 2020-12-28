3a116c50a61684fdca5716cd32b7630387a2f002
soldiii <66373119+soldiii@users.noreply.github.com>
2020-12-28 19:26:34 +0700

    Update StudyGroup.h

--- StudyGroup.h
+++ StudyGroup.h
@@ -60,7 +60,7 @@ class StudyGroup {
 private:
 	string name; // название группы/класса
 	vector<ScheduleStruct> schedule; // расписание
-	void CheckAdd(const Type&, const vector<StudyGroup*>&, const WeekType&, const Day&, const AudienceType&, const size_t&, const string&, const string&, const string&); // проверка данных занятия на правильность
+	void CheckAdd(const Type&, const vector<StudyGroup*>&, const WeekType&, const Day&, const AudienceType&, const size_t&, const string&, const string&, const string&) const; // проверка данных занятия на правильность
 public:
 	void Add(const Type& ,const vector<StudyGroup*>&, const WeekType&, const Day&, const AudienceType&, const size_t&, const string&, const string&, const string&); // добавление данных занятия в расписание
 	string GetName() const ;
