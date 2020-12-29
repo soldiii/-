86e018288648cf2b344e84eb27b4156673a10dc7
soldiii <66373119+soldiii@users.noreply.github.com>
2020-12-29 23:00:46 +0700

    Update School.h

--- School.h
+++ School.h
@@ -6,7 +6,7 @@ private:
 	vector <StudyGroup*> classes; // вектор указателей на классы школы
 	map<string, vector<TeacherStruct>> TeacherSchedule; //расписание учителей
 public:
-	void AddToSchedule(StudyGroup*, const WeekType&, const Day&, const size_t&, const AudienceType&, const string&, const string&, const string&); // добавление занятия в расписание класса 
+	void AddToSchedule(StudyGroup*,WeekType,Day,size_t,AudienceType, const string&, const string&, const string&); // добавление занятия в расписание класса 
 	void Add(StudyGroup*);//добавление указателя класса в вектор указателей классов
 	vector <StudyGroup*> GetStudyGroups() const override;
     Type GetType() const override;
