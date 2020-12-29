7747b374c9c36984e5f5d2cecbc6ddcedae977d3
soldiii <66373119+soldiii@users.noreply.github.com>
2020-12-29 23:04:33 +0700

    Update University.h

--- University.h
+++ University.h
@@ -7,14 +7,14 @@ private:
 	map < WeekType,vector<pair < ScheduleStruct,size_t>>> count_audiences; // словарь для подсчета групп в аудитории
 	map<string, AudienceType> audiences; // словарь, соотносящий аудиторию с ее типом
 	map<string, vector<TeacherStruct>> TeacherSchedule; // расписание преподавателей
-	void AddAudience(const AudienceType&, const string&); // добавить аудиторию и присвоить ей тип
+	void AddAudience(AudienceType, const string&); // добавить аудиторию и присвоить ей тип
 public:
 	map < WeekType, vector<pair < ScheduleStruct, size_t>>> GetCountAudiences() const; // для юнит-тестов
 	map<string, vector<TeacherStruct>> GetTeacherSchedule() const; // для юнит-тестов
 	void Add(StudyGroup*) override; // добавление указателя группы в вектор указателей групп
 	vector <StudyGroup*> GetStudyGroups() const override; 
 	Type GetType() const override;
-	void AddToSchedule(StudyGroup*, const WeekType&, const Day&, const size_t&, const AudienceType&, const string&, const string&, const string&); // добавление занятия в расписание группы 
-	void Delete(const size_t&); // удаление группы
+	void AddToSchedule(StudyGroup*,WeekType,Day,size_t,AudienceType, const string&, const string&, const string&); // добавление занятия в расписание группы 
+	void Delete(size_t); // удаление группы
 	University(const string& name) :Educational_establishment(name) {};
 };
